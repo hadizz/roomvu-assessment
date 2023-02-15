08d6141a6a7360bc2e83fbcf038f496355598225
Hadi Zare <hadizareoriginal@gmail.com>
2023-02-15 21:46:56 +0330

    fix: remove any layout type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,14 @@ import type {AppProps} from 'next/app'
 import MainLayout from "@/pages/layout/MainLayout/MainLayout";
 import {NextComponentType, NextPageContext} from "next/dist/shared/lib/utils";
 import React from "react";
+import {ReactChildren} from "@/types";
 
 export interface RoomvuAppProps extends Omit<AppProps, 'Component'> {
-    Component: NextComponentType<NextPageContext, any, any> & { Layout: JSX.Element }
+    Component: NextComponentType<NextPageContext, any, any> & { Layout: React.FC<ReactChildren> }
 }
 
 export default function App({Component, pageProps}: RoomvuAppProps) {
-    const Layout = (Component.Layout || MainLayout) as any;
+    const Layout = Component.Layout || MainLayout;
 
     return (
         <Layout>
