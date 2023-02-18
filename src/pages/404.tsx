397cf766efb3d64ac406985649540e3d149e5c1f
Hadi Zare <hadizareoriginal@gmail.com>
2023-02-18 19:14:00 +0330

    feat: handle error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error404 = () => (
+    <div style={{minHeight: '50vh'}}>
+        Route Not found, please refresh the page or contact site support
+    </div>
+);
+
+export default Error404;
---
+++ src/pages/500.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error500 = () => (
+    <div style={{minHeight: '50vh'}}>
+        don&apos;t panic! we&apos;re working on it.
+    </div>
+);
+
+export default Error500;
